5a763bcb2af4929cb2db6f302fdb443836237144
Marcin Wałachowski <s19541@pjwstk.edu.pl>
2020-03-16 11:47:49 +0100

    z lekcji

---
+++ Cwiczenia3/Cwiczenia3/Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cwiczenia3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        public string GetStudent()
+        {
+            return "siema";
+        }
+    }
+}
