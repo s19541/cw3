940fbf00df3eaa27b714cc737a1c79991fea4f89
Marcin Wałachowski <s19541@pjwstk.edu.pl>
2020-04-06 11:45:40 +0200

    z lekcji (cw 6)

---
+++ Cwiczenia3/Cwiczenia3/MiddleWares/LoggingMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using Cwiczenia3.Services;
+namespace Cwiczenia3.MiddleWares
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext, DAL.IDbService service)
+        {
+            if (httpContext.Request != null)
+            {
+                string sciezka = httpContext.Request.Path; //"weatherforecast/cos"
+                string querystring = httpContext.Request?.QueryString.ToString();
+                string metoda = httpContext.Request.Method.ToString();
+                string bodyStr = "";
+
+                using (StreamReader reader
+                 = new StreamReader(httpContext.Request.Body, Encoding.UTF8, true, 1024, true))
+                {
+                    bodyStr = await reader.ReadToEndAsync();
+                }
+
+                //logowanie do pliku
+            }
+
+            await _next(httpContext);
+        }
+
+
+    }
+}
--- Cwiczenia3/Cwiczenia3/Startup.cs
+++ Cwiczenia3/Cwiczenia3/Startup.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+
+
 
 namespace Cwiczenia3
 {
@@ -29,6 +32,8 @@ namespace Cwiczenia3
             services.AddSingleton<Services.IStudentsDbService,Services.SqlServerDbService>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
+            //services.AddTransient<DAL.IDbService, Services.SqlServerDbService>();
+            //services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,8 +48,39 @@ namespace Cwiczenia3
                 app.UseHsts();
             }
 
+            //app.UseMiddleware<MiddleWares.LoggingMiddleware>();
             app.UseHttpsRedirection();
             app.UseMvc();
+            app.Use(async (context, next) =>
+            {
+
+                if (!context.Request.Headers.ContainsKey("Index"))
+                {
+                    context.Response.StatusCode = Microsoft.AspNetCore.Http.StatusCodes.Status401Unauthorized;
+                    await context.Response.WriteAsync("Nie podales loginu i hasla");
+                    return;
+                }
+                else
+                {
+                    ////Sprawdzamy w bazie czy istnieje (SqlConnection)
+                    string index = context.Request.Headers["Index"].ToString();
+                    //...
+                    //new SqlConnection()
+                    //dbService.CheckIndexNumber(index);
+                    //401 - return
+                }
+
+                await next();
+            });
+            //app.UseRouting();
+
+           // app.UseAuthorization();
+
+           // app.UseEndpoints(endpoints =>
+           // {
+           //     endpoints.MapControllers();
+           // });
+            
         }
     }
 }
